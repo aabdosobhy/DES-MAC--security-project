223e65072f088a7de2eb45546c723fcae7179c0d
aabdosobhy <aabdosobhy@gmail.com>
2019-04-28 05:58:46 +0200

    adding mode blocks

--- DES.py
+++ DES.py
@@ -39,10 +39,11 @@ class desModes():
         return array
 
     def bitsToString(array): #Recreate the string from the bit array
-        res = ''.join([chr(int(y,2)) for y in [''.join([str(x) for x in _bytes]) for _bytes in  nsplit(array,8)]])   
+        res = ''.join([chr(int(y,2)) for y in 
+            [''.join([str(x) for x in _bytes]) for _bytes in  nsplit(array,8)]])   
         return res
         
-    def desECB(self, plainText):
+    def desECB_Enc(self, plainText):
         result = list()
         plainText = self.stringToBits(plainText)
         desECB=DES.new(self.key, DES.MODE_ECB)   
@@ -53,12 +54,22 @@ class desModes():
             ciph=desECB.encrypt(block)
             result.append(ciph)
         return result
-    
 
-    def desCBC(self, plainText, IV):
+    def desECB_Dec(self, plainText):
         result = list()
-        plainText = self.stringToBits(plainText)
-        desECB=DES.new(self.key, DES.MODE_ECB)        
+        desECB=DES.new(self.key, DES.MODE_ECB)   
+        textBlocks = self.splitMessage(plainText)
+        for block in textBlocks:
+            if len(block) < blockSize:
+                block = self.padBlock(block)
+            dciph=desECB.decrypt(block)
+            result.append(dciph)
+        return result
+       
+
+    def desCBC_Enc(self, plainText, IV):
+        result = list()
+        desECB = DES.new(self.key, DES.MODE_ECB)        
         textBlocks = self.splitMessage(plainText)
         b_no=0
         for block in textBlocks: #Loop over all the blocks of data               
@@ -66,27 +77,48 @@ class desModes():
             if len(block) < blockSize:
                 block = self.padBlock(block)
             if b_no == 0:
-                block = self.xor(IV,block)
+                block = self.xor(IV, block)
             else:
-                block = self.xor(result[b_no-1],block)
-            ciph=desECB.encrypt(block)
+                block = self.xor(result[b_no-1], block)
+            ciph = desECB.encrypt(block)
             result.append(ciph)
+        b_no += 1
+        return result
+
+    def desCBC_Dec(self, plainText, IV):
+        result = list()
+        desECB=DES.new(self.key, DES.MODE_ECB)        
+        textBlocks = self.splitMessage(plainText)
+        for i in range (0 , len(textBlocks))
+        #for block in textBlocks: #Loop over all the blocks of data
+            block = textBlocks[i]
+            if len(block) < blockSize:
+                block = self.padBlock(block)
+            dciph = desECB.decrypt(block)
+            if i == 0:
+                dciph = self.xor(IV, dciph)
+            else:
+                dciph = self.xor(dciph, textBlocks[i-1])
+            result.append(dciph)
 
         return result
     
-    def des_CFB(self):
-        # TODO
+    def des_CFB_Enc(self):
+        #TODO: 
         return
     
-    def des_OFB(self, plainText, Nonce):
+    def des_CFB_Dec(self):
+        #TODO: 
+        return
+    
+    def des_OFB_Enc(self, plainText, Nonce):
         result = list()
         nonceList = list()
-        plainText = self.stringToBits(plainText)
         Nonce = self.stringToBits(Nonce)
         nonceList.append(Nonce)
         desECB = DES.new(self.key, DES.MODE_ECB)        
         textBlocks = self.splitMessage(plainText)
-        b_no=1
+        b_no = 1
         for block in textBlocks: #Loop over all the blocks of data               
             block = self.stringToBits(block)#Convert the block in bit array
             if len(block) < blockSize:
@@ -95,13 +127,54 @@ class desModes():
             nonceList.append(ciph)
             ciph = self.xor(ciph,block)
             result.append(ciph)
+            b_no += 1
+        return result
+
+    def des_OFB_Dec(self, plainText, Nonce):
+        result = list()
+        nonceList = list()
+        Nonce = self.stringToBits(Nonce)
+        nonceList.append(Nonce)
+        desECB = DES.new(self.key, DES.MODE_ECB)        
+        textBlocks = self.splitMessage(plainText)
+        b_no = 1
+        for block in textBlocks: #Loop over all the blocks of data               
+            if len(block) < blockSize:
+                block = self.padBlock(block)
+            dciph=desECB.decrypt(nonceList[b_no -1])
+            nonceList.append(dciph)
+            dciph = self.xor(dciph, block)
+            result.append(dciph)
+            b_no += 1
         return result
     
     
-    def des_CNT(self):
-        # TODO
-        return
-        
+    def des_CNT_Enc(self, plainText, Count=0):
+        result = list()
+        desECB = DES.new(self.key, DES.MODE_ECB)        
+        textBlocks = self.splitMessage(plainText)
+        for block in textBlocks: #Loop over all the blocks of data               
+            block = self.stringToBits(block)#Convert the block in bit array
+            if len(block) < blockSize:
+                block = self.padBlock(block)
+            ciph=desECB.encrypt(Count)
+            ciph = self.xor(ciph,block)
+            result.append(ciph)
+            count+=1
+        return result 
+    
+    def des_CNT_Dec(self, plainText, Count=0):
+        result = list()
+        desECB = DES.new(self.key, DES.MODE_ECB)        
+        textBlocks = self.splitMessage(plainText)
+        for block in textBlocks: #Loop over all the blocks of data               
+            if len(block) < blockSize:
+                block = self.padBlock(block)
+            dciph = desECB.decrypt(Count)
+            dciph = self.xor(dciph, block)
+            result.append(dciph)
+            count+=1
+        return result 
         
 '''       
 if __name__ == '__main__':
