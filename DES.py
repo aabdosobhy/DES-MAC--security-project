22b2cf5d0b1d9e0f621fb49954b7e10cb54f0dc8
aabdosobhy <aabdosobhy@gmail.com>
2019-04-28 05:34:56 +0200

    merging desfile

--- DES.py
+++ DES.py
@@ -18,9 +18,21 @@ class desModes():
         block += padLen * chr(padLen)
         return block
     
+    def stringToBits(text):#Convert a string into a list of bits
+        array = list()
+        for char in text:
+            binval = binvalue(char, 8)#Get the char value on one byte
+            array.extend([int(x) for x in list(binval)]) #Add the bits to the final list
+        return array
+
+    def bitsToString(array): #Recreate the string from the bit array
+        res = ''.join([chr(int(y,2)) for y in [''.join([str(x) for x in _bytes]) for _bytes in  nsplit(array,8)]])   
+        return res
+        
     def desECB(self, plainText):
         result = list()
-        desECB=DES.new(plainText, DES.MODE_ECB)
+        plainText = self.stringToBits(plainText)
+        desECB=DES.new(self.key, DES.MODE_ECB)   
         textBlocks = self.splitMessage(plainText)
         for block in textBlocks:
             if len(block) < blockSize:
@@ -28,40 +40,51 @@ class desModes():
             ciph=desECB.encrypt(block)
             result.append(ciph)
         return result
- 
-    def desCBC(self, text_blocks, action, IV):
-        result = list()
-        b_no=0
-        if action == ENCRYPT:
-            for block in text_blocks: #Loop over all the blocks of data               
-                block = string_to_bit_array(block)#Convert the block in bit array
-                if b_no == 0:
-                    block = self.xor(IV,block)
-                else:
-                    block = self.xor(result[b_no-1],block)
-                d, g = self.desCalc(block)
-                b_no+=1
-                result += self.permut(d+g, PI_1) #Do the last permut and append the result to result
+    
 
-        else:
-            for block in text_blocks:#Loop over all the blocks of data
-                block = string_to_bit_array(block)#Convert the block in bit array
-                d, g = self.desCalc(block)
-                if b_no == 0:
-                    block = self.xor(IV,block)
-                else:
-                    block = self.xor(result[b_no-1],block)
-                b_no+=1
-                result += self.permut(d+g, PI_1) #Do the last permut and append the result to result
-       
+    def desCBC(self, plainText, IV):
+        result = list()
+        plainText = self.stringToBits(plainText)
+        desECB=DES.new(self.key, DES.MODE_ECB)        
+        textBlocks = self.splitMessage(plainText)
+        b_no=0
+        for block in textBlocks: #Loop over all the blocks of data               
+            block = self.stringToBits(block)#Convert the block in bit array
+            if len(block) < blockSize:
+                block = self.padBlock(block)
+            if b_no == 0:
+                block = self.xor(IV,block)
+            else:
+                block = self.xor(result[b_no-1],block)
+            ciph=desECB.encrypt(block)
+            result.append(ciph)
+
         return result
     
     def des_CFB(self):
         # TODO
         return
-    def des_OFB(self):
-        # TODO
-        return
+    
+    def des_OFB(self, plainText, Nonce):
+        result = list()
+        nonceList = list()
+        plainText = self.stringToBits(plainText)
+        Nonce = self.stringToBits(Nonce)
+        nonceList.append(Nonce)
+        desECB = DES.new(self.key, DES.MODE_ECB)        
+        textBlocks = self.splitMessage(plainText)
+        b_no=1
+        for block in textBlocks: #Loop over all the blocks of data               
+            block = self.stringToBits(block)#Convert the block in bit array
+            if len(block) < blockSize:
+                block = self.padBlock(block)
+            ciph=desECB.encrypt(nonceList[b_no-1])
+            nonceList.append(ciph)
+            ciph = self.xor(ciph,block)
+            result.append(ciph)
+        return result
+    
+    
     def des_CNT(self):
         # TODO
         return
